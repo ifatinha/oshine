5dbce50eea95bcd6c5f08a1176435852df81c0d8
Fatima Ferreira <ifatinha99@gmail.com>
2024-11-20 21:58:05 -0300

    header refatorado

--- assets/script/modules/updateMenuLinks.js
+++ assets/script/modules/updateMenuLinks.js
@@ -1,14 +1,16 @@
 export const updateMenuLinks = () => {
-  const links = document.querySelectorAll("nav .header-menu_desktop a");
+  const links = document.querySelectorAll(".navbar-list a");
   const { pathname } = window.location;
 
   if (!links || links.length === 0) return;
 
+  links[0].classList.add("activated");
+
   links.forEach((link) => {
-    link.classList.remove("ativo");
+    link.classList.remove("activated");
 
     if (link.pathname === pathname) {
-      link.classList.add("ativo");
+      link.classList.add("activated");
     }
   });
 };
