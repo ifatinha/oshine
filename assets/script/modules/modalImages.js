e8795cb25da3e5d968a272b16206eefacaf288cd
Fatima Ferreira <ifatinha99@gmail.com>
2024-12-03 19:22:18 -0300

    modal gallery

--- assets/script/modules/modalImages.js
+++ assets/script/modules/modalImages.js
@@ -1,15 +1,31 @@
 export const openModalImage = () => {
   $(document).ready(function () {
-    $("#imageCounter").text("Quantidade de imagens");
+    const images = $(".about__gallery-item");
+    $("#NumberImages").text(`0${images.length}`);
+
+    const createImage = (image) => {
+      const imageContainer = $(".image__container");
+      imageContainer.prepend(image);
+    };
+
+    const removeImage = () => {
+      const image = $(".image__container img");
+      image.remove();
+    };
 
     //Abrir modal clicando no item das imagens;
-    $(".about__gallery-item").on("click", function (idx) {
+    $(images).on("click", function () {
       $("#modalImage").addClass("modal__active");
+      $("#imageCounter").text($(this).attr("data-gallery"));
+      const imageClone = $(this).clone().children()[0];
+      createImage(imageClone);
     });
 
+    // Fechar o modal
     $("#closeModalImage").on("click", function (event) {
       event.preventDefault();
       $("#modalImage").removeClass("modal__active");
+      removeImage();
     });
   });
 };
